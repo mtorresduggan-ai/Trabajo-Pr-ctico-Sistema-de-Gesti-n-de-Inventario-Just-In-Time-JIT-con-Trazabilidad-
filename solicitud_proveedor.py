6865fefb6fa5411dcc867528a25cb7823fdad34d
mb-arocena <marocena@itba.edu.ar>
2026-08-27 20:01:47 -0300

    ultimo cambio del 27

--- solicitud_proveedor.py
+++ solicitud_proveedor.py
@@ -9,3 +9,26 @@ class Solicitud_proveedor:
         self.cant_materiales = cant_materiales
 
         Solicitud_proveedor.todos.append(self)
+
+    def cambiar_fecha_emision(self, nueva_fecha):
+        self.fecha_emision = nueva_fecha
+
+    def cambiar_cantidad(self, nueva_cantidad):
+        self.cant_materiales = nueva_cantidad
+
+    def cambiar_proveedor(self, nuevo_proveedor):
+        self.proveedor = nuevo_proveedor
+
+    def agregar_material(self, material):
+        self.materiales.append(material)
+
+    def quitar_material(self, material):
+        self.materiales.remove(material)
+
+    def informar(self):
+        return 'ID Solicitud: ' + str(self.id_solicitud) + ' Proveedor: ' + self.proveedor.nombre + ' Fecha de emision: ' + str(self.fecha_emision) + ' Cantidad de materiales: ' + str(self.cant_materiales)
+
+    @classmethod
+    def informar_todos(cls):
+        return cls.todos
+    
